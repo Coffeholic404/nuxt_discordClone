06cffb01dfb76c8ac8d76803d6c8e340ec1d274f
Mohammed Taqi <taqealibade@gmail.com>
2024-03-21 16:50:26 +0300

    adding shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"]
+  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  alias: {
+    pinia: process.env.NODE_ENV === 'production' ? '/node_modules/pinia/dist/pinia.mjs' : '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+},
 })

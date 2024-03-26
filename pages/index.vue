37d7715ea9c4d619d6845126d39c99ab6a0d9fdb
Mohammed Taqi <taqealibade@gmail.com>
2024-03-26 23:06:06 +0300

    folders setup

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p class=" font-bold text-3xl text-indigo-500">hi</p>
+        <Button 
+        variant="destructive"
+        >Button</Button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
